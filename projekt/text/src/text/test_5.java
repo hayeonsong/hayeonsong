25ecb7075f27c3a219e93289fa6f97b336f270de
hayeonsong <yon3265@gmail.com>
2018-11-07 02:56:25 +0900

    test

---
+++ projekt/text/src/text/test_5.java
@@ -0,0 +1,20 @@
+package text;
+
+public class test_5 {
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		
+		System.out.println(" x > y : " + (x > y)); 
+		System.out.println(" x < y : " + (x < y));
+		System.out.println(" x >= y : " + (x >= y));
+		System.out.println(" x <= y : " + (x <= y ));
+		System.out.println(" x == y : " + (x == y ));
+		System.out.println(" x != y : " + ( x!= y ));
+	
+	 x = 10;
+	 System.out.println(" ++x : " + ( ++x));
+	 
+	}
+}
